8bc47a19831f44407087afed068f198b5e8140aa
mjalucero <malucero@up.edu.ph>
2024-04-30 00:30:09 +0800

    successfully fetch questions and randomly select number of questions for level/session. updates question after right or wrong answer.

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./hooks/Auth.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>
+  // </React.StrictMode>
 );
--- src/pages/Level.jsx
+++ src/pages/Level.jsx
@@ -3,8 +3,54 @@ import { useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/Auth";
 import { getUserInfo, getQuestionsPerLevel } from "../hooks/Getters";
 import { supabase } from "../supabaseClient";
+import { useImmerReducer } from "use-immer";
 
 import Header from "../components/Header";
+
+function ourReducer(draft, action) {
+  switch (action.type) {
+    case "guessAttempt":
+      if (action.value === "true") {
+        draft.score++;
+        draft.opponentHealth -= 15;
+      } else if (action.value === "false") {
+        draft.playerHealth -= 15;
+      }
+
+      console.log("Score: " + draft.score);
+      console.log("Player: " + draft.playerHealth);
+      console.log("Opponent: " + draft.opponentHealth);
+      draft.currentQuestion = getCurrentQuestion();
+      return;
+    case "startPlaying":
+      draft.playing = true;
+      draft.score = 0;
+      draft.playerHealth = 100;
+      draft.opponentHealth = 100;
+      draft.currentQuestion = getCurrentQuestion();
+    case "addToRandomQuestionsArr":
+      draft.randomQuestions = draft.randomQuestions.concat(action.value);
+      return;
+  }
+
+  function getCurrentQuestion() {
+    if (draft.currentQuestion) {
+      draft.randomQuestions = draft.randomQuestions.slice(1);
+    }
+    console.log(draft.randomQuestions.length);
+    console.log(draft.randomQuestions);
+    return draft.randomQuestions[0];
+  }
+}
+
+const initialState = {
+  playing: false,
+  randomQuestions: [],
+  score: 0,
+  currentQuestion: null,
+  playerHealth: 100,
+  opponentHealth: 100,
+};
 
 export default function Level() {
   const location = useLocation();
@@ -14,9 +60,21 @@ export default function Level() {
   const [userInfo, setUserInfo] = useState(null);
   const [username, setUsername] = useState(null);
   const [pageTitle, setPageTitle] = useState(null);
-  const [levelData, setLevelData] = useState([]);
+  // const [levelData, setLevelData] = useState([]);
 
   const levelid = "L" + location.state.num;
+
+  const [currQuestion, setCurrQuestion] = useState(null);
+  const [btn1Txt, setBtn1Txt] = useState(null);
+  const [btn1Val, setBtn1Val] = useState(null);
+  const [btn2Txt, setBtn2Txt] = useState(null);
+  const [btn2Val, setBtn2Val] = useState(null);
+  const [btn3Txt, setBtn3Txt] = useState(null);
+  const [btn3Val, setBtn3Val] = useState(null);
+  const [btn4Txt, setBtn4Txt] = useState(null);
+  const [btn4Val, setBtn4Val] = useState(null);
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
   const getPageTitle = () => {
     switch (location.state.num) {
@@ -65,68 +123,133 @@ export default function Level() {
     }
   };
 
-  const setGame = (data) => {
-    console.log(data);
+  const setLevelQuestions = (data) => {
+    const sessionQuestionCount = data.length / 2;
+    var questionCount = 0;
+
+    const questions = [];
+
+    while (questionCount < sessionQuestionCount) {
+      let randomIndex = Math.floor(Math.random() * data.length);
+      let randomElement = data[randomIndex];
+
+      if (!questions.includes(randomElement)) {
+        questions.push(randomElement);
+        questionCount++;
+      } else {
+        continue;
+      }
+    }
+
+    return questions;
+    // setQuestions(questions);
+
+    // setCurrQuestion(data[questionCount].question);
+
+    // setBtn1Txt(JSON.parse(data[questionCount].choice1).c);
+    // setBtn1Val(JSON.parse(data[questionCount].choice1).v.toString());
+    // setBtn2Txt(JSON.parse(data[questionCount].choice2).c);
+    // setBtn2Val(JSON.parse(data[questionCount].choice2).v.toString());
+    // setBtn3Txt(JSON.parse(data[questionCount].choice3).c);
+    // setBtn3Val(JSON.parse(data[questionCount].choice3).v.toString());
+    // setBtn4Txt(JSON.parse(data[questionCount].choice4).c);
+    // setBtn4Val(JSON.parse(data[questionCount].choice4).v.toString());
   };
+
+  const handleChoiceClick = (value) => {};
 
   useEffect(() => {
     getPageTitle();
 
-    getUserInfo(user.id).then(function (res) {
+    getUserInfo(session?.user.id).then(function (res) {
       setUserInfo(res);
       setUsername(res?.username);
     });
 
-    getQuestionsPerLevel(levelid).then(function (res) {
-      setLevelData(res);
-      setGame(res);
-    });
+    async function go() {
+      try {
+        const levelData = await getQuestionsPerLevel(levelid);
+        const levelQuestions = setLevelQuestions(levelData);
+        dispatch({ type: "addToRandomQuestionsArr", value: levelQuestions });
+        dispatch({ type: "startPlaying" });
+      } catch (err) {
+        console.error("Error: ", err.message);
+      }
+    }
+
+    go();
+
+    return () => {};
   }, []);
 
   return (
-    <div className="w-screen h-screen place-content-end bg-slate-900">
-      <Header pageTitle={pageTitle} username={username} />
+    <div>
+      {session && state.playing && (
+        <div className="w-screen h-screen place-content-end bg-slate-900">
+          <Header pageTitle={pageTitle} username={username} />
 
-      <div className="flex flex-col place-items-center w-screen h-2/3 ">
-        <div className="bg-white w-screen h-1/4 mb-4">
-          {/* <span>{currQuestion}</span> */}
-        </div>
-        <div className="flex relative flex-row  h-2/3 w-screen">
-          <div className="relative bg-orange-300 w-64 h-full left-40"></div>
-          <div className="absolute bg-yellow-300 w-64 h-full inset-y-0 right-40"></div>
-        </div>
-      </div>
+          <div className="flex flex-col place-items-center w-screen h-2/3 ">
+            <div className="bg-white w-screen h-1/4 mb-4">
+              <span>{state.currentQuestion.question}</span>
+            </div>
+            <div className="flex relative flex-row  h-2/3 w-screen">
+              <div className="relative bg-orange-300 w-64 h-full left-40"></div>
+              <div className="absolute bg-yellow-300 w-64 h-full inset-y-0 right-40"></div>
+            </div>
+          </div>
 
-      <div className="flex flex-row  h-1/4 w-screen place-content-center">
-        <button
-          className="bg-white w-1/4 h-30 rounded-xl m-3"
-          // value={button1Val}
-          // onClick={() => handleChoiceClick(button1Val)}
-        >
-          {/* <span>{button1Txt}</span> */}
-        </button>
-        <button
-          className="bg-white w-1/4 h-30 rounded-xl m-3"
-          // value={button2Val}
-          // onClick={() => handleChoiceClick(button2Val)}
-        >
-          {/* <span>{button2Txt}</span> */}
-        </button>
-        <button
-          className="bg-white w-1/4 h-30 rounded-xl m-3"
-          // value={button3Val}
-          // onClick={() => handleChoiceClick(button3Val)}
-        >
-          {/* <span>{button3Txt}</span> */}
-        </button>
-        <button
-          className="bg-white w-1/4 h-30 rounded-xl m-3"
-          // value={button4Val}
-          // onClick={() => handleChoiceClick(button4Val)}
-        >
-          {/* <span>{button4Txt}</span> */}
-        </button>
-      </div>
+          <div className="flex flex-row  h-1/4 w-screen place-content-center">
+            <button
+              className="bg-white w-1/4 h-30 rounded-xl m-3"
+              value={JSON.parse(state.currentQuestion.choice1).v.toString()}
+              onClick={() =>
+                dispatch({
+                  type: "guessAttempt",
+                  value: JSON.parse(state.currentQuestion.choice1).v.toString(),
+                })
+              }
+            >
+              <span>{JSON.parse(state.currentQuestion.choice1).c}</span>
+            </button>
+            <button
+              className="bg-white w-1/4 h-30 rounded-xl m-3"
+              value={JSON.parse(state.currentQuestion.choice2).v.toString()}
+              onClick={() =>
+                dispatch({
+                  type: "guessAttempt",
+                  value: JSON.parse(state.currentQuestion.choice2).v.toString(),
+                })
+              }
+            >
+              <span>{JSON.parse(state.currentQuestion.choice2).c}</span>
+            </button>
+            <button
+              className="bg-white w-1/4 h-30 rounded-xl m-3"
+              value={JSON.parse(state.currentQuestion.choice3).v.toString()}
+              onClick={() =>
+                dispatch({
+                  type: "guessAttempt",
+                  value: JSON.parse(state.currentQuestion.choice3).v.toString(),
+                })
+              }
+            >
+              <span>{JSON.parse(state.currentQuestion.choice3).c}</span>
+            </button>
+            <button
+              className="bg-white w-1/4 h-30 rounded-xl m-3"
+              value={JSON.parse(state.currentQuestion.choice4).v.toString()}
+              onClick={() =>
+                dispatch({
+                  type: "guessAttempt",
+                  value: JSON.parse(state.currentQuestion.choice4).v.toString(),
+                })
+              }
+            >
+              <span>{JSON.parse(state.currentQuestion.choice4).c}</span>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
