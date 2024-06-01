3895429213abde8198987a968c536f1615522b09
mjalucero <malucero@up.edu.ph>
2024-06-01 21:26:47 +0800

    updated link to deployed site

--- src/initSocket.jsx
+++ src/initSocket.jsx
@@ -5,7 +5,7 @@ let socket;
 
 export const initializeSocket = () => {
   if (!socket) {
-    socket = io("https://456-server.glitch.me/", { transports: ["websocket"] });
+    socket = io("https://456-mu.vercel.app/", { transports: ["websocket"] });
   }
 
   return socket;
